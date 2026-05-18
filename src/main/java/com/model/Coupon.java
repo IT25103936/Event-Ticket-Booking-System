6f5f8ae8d0152063b0e9bba79435b4a248e13d84
IT25103935 <it25103935@my.sliit.lk>
2026-05-18 12:45:26 +0530

    Class file added

---
+++ src/main/java/com/model/Coupon.java
@@ -0,0 +1,44 @@
+package com.model;
+
+
+public class Coupon {
+
+
+    private int id;
+    private String code;
+    private int discount;
+    private String status;
+
+
+    public Coupon(int id, String code, int discount, String status) {
+        this.id = id;
+        this.code = code;
+        this.discount = discount;
+        this.status = status;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+
+    public int getDiscount() {
+        return discount;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
