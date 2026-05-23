6e5016533950e583221382706652599066cca096
IT25103935 <it25103935@my.sliit.lk>
2026-05-23 22:14:45 +0530

    update Coupon.java

--- src/main/java/com/model/Coupon.java
+++ src/main/java/com/model/Coupon.java
@@ -1,14 +1,11 @@
 package com.model;
 
-
 public class Coupon {
-
 
     private int id;
     private String code;
     private int discount;
     private String status;
-
 
     public Coupon(int id, String code, int discount, String status) {
         this.id = id;
@@ -17,26 +14,21 @@ public class Coupon {
         this.status = status;
     }
 
-
     public int getId() {
         return id;
     }
-
 
     public String getCode() {
         return code;
     }
 
-
     public int getDiscount() {
         return discount;
     }
 
-
     public String getStatus() {
         return status;
     }
-
 
     public void setStatus(String status) {
         this.status = status;
